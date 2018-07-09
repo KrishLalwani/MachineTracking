5fcfdce93be1f08f0674943db657cc228cbbfcf4
Aadityarathour <40973276+Aadityarathour@users.noreply.github.com>
2018-07-09 15:30:57 +0530

    final supplier

--- addmc.php
+++ addmc.php
@@ -65,12 +65,23 @@
                 if($_POST['alert-server-new']=='1')
                 {
                     //This will insert in company if alert server new is 1 it is alert that will be issued if other device is selected. First entry will be made then id will be selected
-                    $req=$pdo->prepare('INSERT INTO company(name) VALUES(:name)');
-                    $req->execute(array(':name'=>$_POST['company2']));
-                    $cmn=$_POST['company2'];
-                }
-                else 
-                    $cmn=$_POST['company'];
+
+                     $read=$pdo->prepare('SELECT * from company where name = :name');
+                    $read->execute(array(':name'=>$_POST['company2']));
+                    $rowr=$read->fetch(PDO::FETCH_ASSOC);
+                     if($rowr == false)
+                    {
+                        $req=$pdo->prepare('INSERT INTO company(name) VALUES(:name)');
+                        $req->execute(array(':name'=>$_POST['company2']));
+                        $cmn=$_POST['company2'];
+                    }
+                    else
+                    {
+                        $cmn=$_POST['company2'];
+                    }
+                    
+
+                  }  
 
                 $req2 = $pdo->prepare("SELECT company_id from company where name = :name");
                 $req2->execute(array(":name" => $cmn));
@@ -86,9 +97,22 @@
                 if($_POST['alert-server-new-supplier']=='1')
                 {
                     //This will insert in company if alert server new is 1 it is alert that will be issued if other device is selected. First entry will be made then id will be selected
-                    $req=$pdo->prepare('INSERT INTO supplier(supname) VALUES(:supname)');
-                    $req->execute(array(':supname'=>$_POST['supplier2']));
-                    $smn=$_POST['supplier2'];
+
+
+                    $req=$pdo->prepare('SELECT * from supplier where supname = :supname');
+                $req->execute(array(':supname'=>$_POST['supplier']));
+                $rowrr=$req->fetch(PDO::FETCH_ASSOC);
+                if($rowrr == false)
+                {
+                    $req=$pdo->prepare('INSERT INTO name(name) VALUES(:name)');
+                    $req->execute(array(':name'=>$_POST['supplier']));
+                    $supname=$_POST['supplier'];    
+                }
+                else
+                {
+                    $supname=$_POST['supplier2'];
+                }
+
                 }
                 else 
                     $smn=$_POST['supplier'];
